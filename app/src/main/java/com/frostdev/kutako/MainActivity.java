2b40060073703123d7daa365b7a8b89ab9a2b974
Fahril <fahrilmaula005@gmail.com>
2024-08-17 22:05:52 +0700

    update regist

--- app/src/main/java/com/frostdev/kutako/MainActivity.java
+++ app/src/main/java/com/frostdev/kutako/MainActivity.java
@@ -13,22 +13,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Animation topAnim, bottomAnim;
-    TextView title;
-
-    private static final int SPLASH = 3500;
+    private static final int SPLASH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-        title = findViewById(R.id.title_splash);
-
-        title.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
